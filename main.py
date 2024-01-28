65c901b7897f98caee369407436ca20272a934a3
AleenaKhan10 <aleenakhanraees40@gmail.com>
2024-01-28 23:46:09 +0500

    fixed the order of the endpoints

--- main.py
+++ main.py
@@ -14,11 +14,12 @@ def get_blogs():
     return 'here is the list of blogs'
 
 
+@app.get('/blog/unpublished')
+def get_unpublished_blogs():
+    return ('unpublished blogs')
+
+
 @app.get('/blog/{id}')
 def get_blog_id(id: int):
     return f'This is the blog with id {id}'
 
-
-@app.get('/blog/unpublished')
-def get_unpublished_blogs():
-    return ('unpublished blogs')
