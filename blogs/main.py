fd74e00792cdae7b2bab3f3de8fadbb25378210f
AleenaKhan10 <aleenakhanraees40@gmail.com>
2024-01-29 12:28:26 +0500

    fixed the imports

--- blogs/main.py
+++ blogs/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 from . import schemas, models
 from database.database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -19,5 +19,46 @@ def get_db():
 
 @app.post('/blog/create')
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
-    return(f'blogs created successfull with title as {request.title}')
+    blog = models.Blog(title = request.title, description = request.description)
+    db.add(blog)
+    db.commit()
+    db.refresh(blog)
+    return blog
+
+
+@app.get('/blog/all')
+def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}', status_code= status.HTTP_201_CREATED)
+def get_blog_from_id(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f'Blog with this id {id} is not found')
+    return blog
+
+
+@app.put('/blog/{id}', status_code= status.HTTP_200_OK)
+def update_blog(id, request : schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"blog with id {id} is not found")
+    else:
+        blog.update(request)
+        db.commit()
+        db.refresh(blog)
+        return blog
     
+    
+@app.put('/blog/{id}', status_code= status.HTTP_200_OK)
+def delete_blog(id, request : schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"blog with id {id} is not found")
+    else:
+        blog.delete(synchronize_session=False)
+        db.commit()
+        db.refresh(blog)
+        return blog
